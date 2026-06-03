e0acb3b48f125c01cf1db43aca157da1555dd343
NaderDhamer <dhamer1995@gmail.com>
2026-06-03 22:43:45 +0200

    fixing terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "EKS Kubernetes version."
   type        = string
-  default     = "1.29"
+  default     = "1.30"
 }
 
 variable "cluster_role_arn" {
